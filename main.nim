e7bb714694b03d9d77c8d87fd5ec1bdd5a95ac01
Stella <claudiavanillatea@gmail.com>
2023-10-05 12:45:49 +0000

    Removed makeProj.bash. All nim code.

--- main.nim
+++ main.nim
@@ -1,18 +1,17 @@
-import osproc
 import std/strutils
 import std/strformat
-var scriptPath = "/home/stella/Documents/projects/nim/harpy/makeProj.bash"
+import std/os
+var directory: string
+var corresponds: string
+let
+    java: string = "//Here goes your code java dev, have fun debugging."
+    python: string = "#Hss hss, im a snake"
+    kotlin: string = "//Kotlin is, fun, right?"
 
-proc runBash(path: string, lang: int): int =
-    var
-        result: int
-    let
-        cmd = fmt"bash {path} {lang}"
-    
-    result = execCmd(cmd)
-    if result != 0:
-        echo "Error executing bash script"
-    return result
+if not dirExists(directory):
+    os.createDir(directory)
+else:
+    echo "Directory already exists"
 
 echo "Welcome user to harpy, a custom tool for all your automatic project creation!"
 echo "-----------"
@@ -22,4 +21,43 @@ echo "Enter the number that corresponds to the language you would like to genera
 
 let selectedLang: int = parseInt(readLine(stdin))
 
-var exitCode = runBash(scriptPath, selectedLang)
+case selectedLang:
+    of 1:
+        corresponds = "java"
+    of 2:
+        corresponds = "py"
+    of 3:
+        corresponds = "kt"
+    else:
+        echo "Invalid choice"
+
+directory = fmt"{corresponds} project"
+createDir(directory)
+
+let fileName = directory & fmt"/main.{corresponds}"
+
+var file: File
+
+if corresponds == "java":
+    if open(file, fileName, fmWrite):
+        echo "File created successfully"
+        write(file, fmt"{java}")
+        close(file)
+    else:
+        echo "Failed to create file"
+elif corresponds == "py":
+    if open(file, fileName, fmWrite):
+        echo "File created successfully"
+        write(file, fmt"{python}")
+        close(file)
+    else:
+        echo "Failed to create file"
+elif corresponds == "kt":
+    if open(file, fileName, fmWrite):
+        echo "File created successfully"
+        write(file, fmt"{kotlin}")
+        close(file)
+    else:
+        echo "Failed to create file"
+else:
+    echo "You made a typo somewhere, whoopsies"
